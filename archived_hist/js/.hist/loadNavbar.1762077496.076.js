864b68762ede8d1ffa63f1893248edba64a7d64b
Giann <giannca@icloud.com>
2025-11-25 22:17:22 -0800

    removed hist things

--- archived_hist/js/.hist/loadNavbar.1762077496.076.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  fetch('/priority/navbar.html')
-    .then(response => response.text())
-    .then(data => {
-      document.body.insertAdjacentHTML('afterbegin', data);
-    })
-    .catch(error => console.error('Error fetching navbar:', error));
-})
